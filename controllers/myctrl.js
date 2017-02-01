feb202b46d4cfa6fac8754a93a4fed38ed4d66fb
bill hoang <billhoang79@gmail.com>
2017-02-01 13:49:28 -0800

    ng model example

---
+++ controllers/myctrl.js
@@ -0,0 +1,13 @@
+app.controller('myCtrl', function($scope) {
+	$scope.name = {
+		first: "Bill",
+		last: "Hoang"
+	}
+})
+
+app.controller('carCtrl', function($scope) {
+	$scope.car = {
+		make: "toyota",
+		color: "blue"
+	}
+})
